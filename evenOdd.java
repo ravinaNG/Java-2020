91aeece0f336d43def2c31b73115a2a8a9b6e364
ravinaNG <ravina18@navgurukul.org>
2020-06-25 23:31:13 +0530

    even or odd question

---
+++ evenOdd.java
@@ -0,0 +1,10 @@
+public class evenOdd{
+    public static void main(String[]args){
+        int num = 7;
+        if(num%2==0){
+            System.out.println("It's a even number.");
+        }
+    }
+}
+
+
