98b2e2808ca218b1e8516fb17a6c581919619694
ravinaNG <ravina18@navgurukul.org>
2020-06-26 00:19:36 +0530

    simple loop

---
+++ simpleLoop.java
@@ -0,0 +1,11 @@
+public class simpleLoop {
+    public static void main(String[] orgs){
+        int starting_num = 1;
+        while(starting_num<=10){
+
+            System.out.print("Ravina ");
+            System.out.println(starting_num);
+            starting_num++;
+        }
+    }
+}
