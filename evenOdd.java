f2e8967681795241109575ecb74cfa91d1a915e9
ravinaNG <ravina18@navgurukul.org>
2020-06-26 00:09:17 +0530

    food manu

--- evenOdd.java
+++ evenOdd.java
@@ -1,9 +1,10 @@
 public class evenOdd{
     public static void main(String[]args){
         int num = 7;
-        if(num%2==0){
+        if(num%2==0)
             System.out.println("It's a even number.");
-        }
+        else
+            System.out.println("It's a odd number.");
     }
 }
 
--- foodMenu.java
+++ foodMenu.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class foodMenu{
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the day you will cook:- ");
+        String day = sc.nextLine();  
+        // System.out.println(day); 
+        if(day.equals("monday")){
+            System.out.println("Rajma chawal :)");
+        }
+        else if(day.equals("sunday")){
+            System.out.println("Biryani");
+        }
+        else if(day.equals("tuesday")){
+            System.out.println("Daal chawal and bhujya");
+        }
+        else{
+            System.out.println("make whatever you want");
+        }
+    }
 }
