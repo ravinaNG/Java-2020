bfd7171b0996a78414b7b88a870060bdcfa7b7d9
ravinaNG <ravina18@navgurukul.org>
2020-07-03 10:39:56 +0530

    how to access objects from one to another class

---
+++ myPackage/accessClass.java
@@ -0,0 +1,6 @@
+package myPackage;
+public class accessClass {
+    public void display(){
+        System.out.println("Hello");
+    }
+}
---
+++ myPackage/secondClass.java
@@ -0,0 +1,8 @@
+package myPackage;
+
+public class secondClass{
+    public static void main(String []args) {
+        accessClass object = new accessClass();
+        object.display();
+    }
+}
