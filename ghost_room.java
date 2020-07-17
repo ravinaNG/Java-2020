d08ae7abd6a91d6d078e5407a411bf5306c2fbc4
ravinaNG <ravina18@navgurukul.org>
2020-07-17 16:51:16 +0530

    not able to do

--- ghost_room.java
+++ ghost_room.java
@@ -1,7 +1,7 @@
 public class ghost_room {
     public static void main(String[] array) {
         int[][] main_list = new int[][]{{5, 2, 3, 0}, 
-                                        {2, 0, 5, 3}, 
+                                        {2, 1, 5, 3}, 
                                         {0, 1, 2, 1}, 
                                         {3, 0, 2, 0}};
         int list_element = 0;
@@ -10,9 +10,14 @@ public class ghost_room {
         int last_list = main_list.length - 1;
 
         while(list_element<=last_list){
-            if(main_list[list_element][small_list_element] != 0 && list_element+2 <= last_list){
-                if(main_list[list_element+2][small_list_element] != 0){
-                    sum = sum + main_list[list_element+1][small_list_element];
+            if(list_element < last_list && list_element > 0){
+                if(main_list[list_element+1][small_list_element] != 0 && main_list[list_element-1][small_list_element] != 0){
+                    sum = sum + main_list[list_element][small_list_element];
+                }
+                if(list_element < last_list && list_element > 0){
+                    if(main_list[0][small_list_element] != 0 && main_list[list_element][small_list_element] != 0){
+                        sum = sum + main_list[list_element][small_list_element];
+                    }
                 }
             }
             if(main_list[1][small_list_element] != 0){
